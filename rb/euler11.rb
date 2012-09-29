72ce7fd3f704a38c331772a811636630a917c445
Joshua Clark <joshua.clark@txstate.edu>
2012-09-28 19:22:04 -0500

    euler11 is correct

--- rb/euler11.rb
+++ rb/euler11.rb
@@ -1,42 +1,76 @@
-file = File.open('../11.txt')
 
-arr = Array.new
+def get_array(file)
+  file = File.open(file)
 
-file.each do |i|
-  arr[arr.size] = i.split ' '
+  arr = Array.new
+
+  file.each do |i|
+    arr[arr.size] = i.split ' '
+  end  
+  arr
 end
 
 
-arr.each do |i| 
-  i.each do |j| 
-    begin
-      j = Integer(j[/([1-9]\d*|^0+$)/])
-    rescue
-      puts 'caught ' + j
+def integerize(arr)
+  0.upto arr.length - 1 do |i| 
+    0.upto arr[i].length - 1 do |j| 
+      arr[i][j] = Integer(arr[i][j][ /([1-9]\d*|^0+$)/ ])
     end
   end
 end
 
-
-col = arr.size
-row = arr[0].size
-
-
-sum = 0
-
-#horizontal test
-0.upto row do |i|
-  0.upto col - 4 do |j|
-    localsum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i][j+3]
-    begin
-      sum = localsum if localsum > sum
-    rescue
-      puts sum
-      puts localsum
-      return
+def horizontal(arr)
+  theproduct = 0
+  0.upto arr.length - 4 do |i|
+    0.upto arr[i].length - 1 do |j|
+      localproduct = arr[i][j] * arr[i + 1][j] * arr[i + 2][j] * arr[i + 3][j]
+      theproduct = localproduct if localproduct >  theproduct
     end
   end
+  theproduct
 end
 
-puts 'sum ' + sum
+def vertical(arr)
+  theproduct = 0
+  0.upto arr.length - 1 do |i|
+    0.upto arr[i].length - 4 do |j|
+      localproduct = arr[i][j] * arr[i][j + 1] * arr[i][j + 2] * arr[i][j + 3]
+      theproduct = localproduct if localproduct > theproduct
+    end
+  end
+  theproduct
+end
 
+def diagonal_right(arr)
+  theproduct = 0
+  0.upto arr.length - 4 do |i|
+    0.upto arr[i].length - 4 do |j|
+      localproduct = arr[i][j] * arr[i + 1][j + 1] * arr[i + 2][j + 2] * arr[i + 3][j + 3]
+      theproduct = localproduct if localproduct > theproduct
+    end
+  end
+  theproduct  
+end
+
+def diagonal_left(arr)
+  theproduct = 0
+  3.upto arr.length - 1 do |i|
+    0.upto arr[i].length - 4 do |j|
+      localproduct = arr[i][j] * arr[i - 1][j + 1] * arr[i - 2][j + 2] * arr[i - 3][j + 3]
+      theproduct = localproduct if localproduct > theproduct
+    end
+  end
+  theproduct  
+end
+
+arr = get_array '../11.txt'
+integerize arr
+
+product = horizontal arr
+vertical_product = vertical arr
+product = vertical_product if vertical_product > product
+diagonal_product = diagonal_right arr
+product = diagonal_product if diagonal_product > product
+diagonal_product = diagonal_left arr
+product = diagonal_product if diagonal_product > product
+puts product
